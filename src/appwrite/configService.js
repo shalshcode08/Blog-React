4b981b79990862b7b72ffbb12d07449084896e42
Somya Shrestha <90999893+shalshcode08@users.noreply.github.com>
2024-10-19 14:10:31 +0530

    Appwrite Configuration Srvice

---
+++ src/appwrite/configService.js
@@ -0,0 +1,129 @@
+import { Account, Client, Databases, Storage, Query, ID } from 'appwrite';
+import conf from '../config/config';
+
+
+export class Service {
+    client = new Client();
+    databases;
+    storage;
+
+    constructor() {
+        this.client
+            .setEndpoint(conf.appwriteUrl)
+            .setProject(conf.appwriteProjectId)
+        this.databases = new Databases(this.client)
+        this.storage = new Storage(this.client)
+    }
+
+    async createPost({ title, slug, content, featuredImage, status, userId }) {
+        try {
+            return await this.databases.createDocument(
+                conf.appwriteDatabaseId,
+                conf.appwriteCollectionId,
+                slug,
+                {
+                    title,
+                    content,
+                    featuredImage,
+                    status,
+                    userId
+                }
+            )
+        } catch (error) {
+            throw error;
+        }
+    }
+
+    async updatePost(slug, { title, content, featuredImage, status }) {
+        try {
+            return await this.databases.updateDocument(
+                conf.appwriteDatabaseId,
+                conf.appwriteCollectionId,
+                slug,
+                {
+                    title,
+                    content,
+                    featuredImage,
+                    status
+                }
+            )
+        } catch (error) {
+            throw error;
+        }
+    }
+
+    async deletePost(slug) {
+        try {
+            await this.databases.deleteDocument(
+                conf.appwriteDatabaseId,
+                conf.appwriteCollectionId,
+                slug
+            )
+            return true;
+        } catch (error) {
+            throw error;
+        }
+    }
+
+
+    async getPost(slug) {
+        try {
+            return await this.databases.getDocument(
+                conf.appwriteDatabaseId,
+                conf.appwriteCollectionId,
+                slug
+            )
+        } catch (error) {
+            throw error
+        }
+    }
+
+
+    async getAllPost(queries = [Query.equal("status", "active")]) {
+        try {
+            return await this.databases.listDocuments(
+                conf.appwriteDatabaseId,
+                conf.appwriteCollectionId,
+                queries,
+            )
+        } catch (error) {
+            throw error;
+        }
+    }
+
+    //file upload
+    async uploadFile(file) {
+        try {
+            return await this.storage.createFile(
+                conf.appwriteBucketId,
+                ID.unique,
+                file,
+            )
+        } catch (error) {
+            throw error;
+        }
+    }
+
+    async deleteFile(fileId) {
+        try {
+            await this.storage.deleteFile(
+                conf.appwriteBucketId,
+                fileId,
+            )
+        } catch (error) {
+            console.log(error);
+            return false;
+        }
+    }
+
+    async getFilePreview(fileId) {
+        return this.storage.getFilePreview(
+            conf.appwriteBucketId,
+            fileId,
+        )
+    }
+}
+
+const service = new Service()
+
+export default service;
