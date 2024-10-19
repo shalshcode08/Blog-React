995ff9287b997e6b111699402a0039f086361cee
Somya Shrestha <90999893+shalshcode08@users.noreply.github.com>
2024-10-19 16:07:10 +0530

    Added redux

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,42 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
+import { useState, useEffect } from 'react'
+import { useDispatch } from 'react-redux'
+import {login, logout} from './store/authSlice'
+import {Header, Footer} from './components/index'
+import { Outlet } from 'react-router-dom';
+import authService from './appwrite/auth'
+
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [loading, setLoading] = useState(true)
+  const dispatch = useDispatch()
 
-  return (
-    <>
-      <h1>Hello World</h1>
-    </>
-  )
+  useEffect(()=>{
+    authService.getCurrentUser()
+    .then((userData)=>{
+      if(userData){
+        dispatch(login({userData}))
+      }
+      else{
+        dispatch(logout())
+      }
+    })
+    .finally(()=>setLoading(false))
+  }, [])
+
+    return !loading ?  (
+      <div className='min-h-screen flex flex-wrap content-between bg-gray-400'>
+        <div className='w-full block'>
+          <Header/>
+          <main>
+            TODO : <Outlet/>
+          </main>
+          <Footer/>
+        </div>
+      </div>
+    )
+    : null 
+
 }
 
 export default App
